63dacb241257d63554dc2275e0c811cee6f1f446
shimizuarata <asone.runteq31@gmail.com>
2023-04-13 11:30:25 +0900

    quest_9の提出

---
+++ week1-2/quest/quest_9/quest_9.rb
@@ -0,0 +1,11 @@
+# 2.両替
+def convert_currency(amount, source_currency, target_currency, conversion_rates)
+    puts (amount / conversion_rates[source_currency] * conversion_rates[target_currency])
+end
+
+conversion_rates = {
+  usd: 1.0,
+  jpy: 110.0,
+  eur: 0.8
+}
+convert_currency(100, :usd, :jpy, conversion_rates)
